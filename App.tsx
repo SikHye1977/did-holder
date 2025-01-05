7aded133d56e65f1855b14e4e5107afd1e8f041d
SikHye1977 <sikhye1977@naver.com>
2025-01-05 20:56:33 +0900

    Initial Setting

---
+++ App.tsx
@@ -0,0 +1,32 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <Text>this is home</Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+});
+
+export default App;
